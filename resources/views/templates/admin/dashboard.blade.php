02e1a7bdd6485107c0e49442b0ac404a6936993f
ffadilaputra <sfkikuk@gmail.com>
2017-01-30 12:07:45 +0700

    Ganti Lokalan Kabeh

--- resources/views/templates/admin/dashboard.blade.php
+++ resources/views/templates/admin/dashboard.blade.php
@@ -2,9 +2,32 @@
 @section('content')
 
 <div class="container">
-    <div class="col-md-6">
-            
-    </div>
+   <div class="col-sm-3">
+   <div class="panel panel-default">
+   <h3>&nbsp;&nbsp;<i class="glyphicon glyphicon-briefcase"></i>&nbsp;Toolbox</h3>
+    <hr>
+      <ul class="nav nav-stacked">
+        <li><a href="#" target="ext"><i class="glyphicon glyphicon-flash"></i> Alerts</a></li>
+        <li><a href="#" target="ext"><i class="glyphicon glyphicon-link"></i> Links</a></li>
+        <li><a href="#" target="ext"><i class="glyphicon glyphicon-list-alt"></i> Reports</a></li>
+        <li><a href="#" target="ext"><i class="glyphicon glyphicon-book"></i> Books</a></li>
+        <li><a href="#" target="ext"><i class="glyphicon glyphicon-briefcase"></i> Tools</a></li>
+        <li><a href="#" target="ext"><i class="glyphicon glyphicon-time"></i> Real-time</a></li>
+        <li><a href="#" target="ext"><i class="glyphicon glyphicon-plus"></i> Advanced..</a></li>
+      </ul>
+      <hr>
+  	</div>
+  	</div>
+
+      <div class="col-sm-9">
+        <h3><i class="glyphicon glyphicon-dashboard"></i> Bootply Dashboard</h3>
+      <hr>  
+        <div class="panel panel-default">
+        <p>
+             This is a responsive dashboard-style layout that uses <a href="http://www.getbootstrap.com">Bootstrap 3</a>. You can use this template as a starting point to create something more unique.
+        </p>
+        </div>
+   </div>
 </div>
 
 @stop
--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -8,10 +8,9 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="{{ elixir('css/app.css') }}">
         <link rel="stylesheet" href="{{ URL::asset('css/custom.css') }}";>
-        <script src="https://use.fontawesome.com/112f3701fa.js"></script>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/css/bootstrap-datepicker3.css">
-        <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/bootstrap-table/1.11.0/bootstrap-table.min.css">
-        <link rel="stylesheet" href="https://cdn.datatables.net/1.10.13/css/dataTables.bootstrap.min.css">
+        <link rel="stylesheet" href="{{ URL::asset('lib/font-awesome/css/font-awesome.min.css') }}";>
+        <link rel="stylesheet" href="{{ URL::asset('lib/bootstrap-datepicker/css/bootstrap-datepicker3.min.css') }}">
+        <link rel="stylesheet" href="{{ URL::asset('lib/bootstrap-datatables/css/dataTables.bootstrap.min.css') }}">
     </head>
     <body>
        @include('templates.header')
@@ -20,15 +19,12 @@
 
         @include('templates.footer')
 
-        <script
-        src="https://code.jquery.com/jquery-3.1.1.min.js"
-        integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8="
-        crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/jqueryui/1.12.1/jquery-ui.js"></script>
-        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/datatables/1.10.13/js/jquery.dataTables.min.js"></script>
-        <script src="https://cdn.datatables.net/1.10.13/js/dataTables.bootstrap.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-datepicker/1.6.4/js/bootstrap-datepicker.min.js"></script>
+        <script src="{{ URL::asset('lib/jquery-3.1.1.min.js') }}"></script>
+        <script src="{{ URL::asset('lib/jquery-ui/jquery-ui.min.js') }}"></script>
+        <script src="{{ URL::asset('lib/bootstrap/bootstrap.min.js') }}"></script>
+        <script src="{{ URL::asset('lib/bootstrap-datatables/js/jquery.dataTables.min.js') }}"></script>
+        <script src="{{ URL::asset('lib/bootstrap-datatables/js/dataTables.bootstrap.min.js') }}"></script>
+        <script src="{{ URL::asset('lib/bootstrap-datepicker/js/bootstrap-datepicker.min.js') }}"></script>
         <script type="text/javascript">
         $(document).ready(function() {
             $('#tg').datepicker({
